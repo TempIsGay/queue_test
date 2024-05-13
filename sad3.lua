4aca0c8853ed38be52c8d4a7682cf8047257f952
TempIsGay <168687539+TempIsGay@users.noreply.github.com>
2024-05-13 16:52:22 -0700

    Update sad3.lua

--- sad3.lua
+++ sad3.lua
@@ -23,28 +23,30 @@ local function ServerHop()
 
 
 	while true do		
-		local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
-		local Server, Next = nil, nil
+		pcall(function()
+			local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+			local Server, Next = nil, nil
 
-		local function ListServers(cursor)
-			local Raw = game:HttpGet(Servers .. ((cursor and "&cursor="..cursor) or ""))
+			local function ListServers(cursor)
+				local Raw = game:HttpGet(Servers .. ((cursor and "&cursor="..cursor) or ""))
 
-			return game:GetService("HttpService"):JSONDecode(Raw)
-		end
+				return game:GetService("HttpService"):JSONDecode(Raw)
+			end
 
-		repeat
-			local Servers = ListServers(Next)
-			Server = Servers.data[math.random(1, (#Servers.data / 3))]
-			Next = Servers.nextPageCursor
-		until Server
+			repeat
+				local Servers = ListServers(Next)
+				Server = Servers.data[math.random(1, (#Servers.data / 3))]
+				Next = Servers.nextPageCursor
+			until Server
 
-		if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
-			pcall(function()
-				game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, Server.id, game:GetService("Players").LocalPlayer)
-			end)
-		end
+			if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+				pcall(function()
+					game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, Server.id, game:GetService("Players").LocalPlayer)
+				end)
+			end
 
-		task.wait(10)
+			task.wait(10)
+		end)
 	end
 end
 
