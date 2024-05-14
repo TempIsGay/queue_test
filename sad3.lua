465d8fc63f41794a74366d96db618c1e0443c1a3
TempIsGay <168687539+TempIsGay@users.noreply.github.com>
2024-05-13 18:02:14 -0700

    Update sad3.lua

--- sad3.lua
+++ sad3.lua
@@ -8,7 +8,7 @@ end
 
 game.StarterGui:SetCore("SendNotification", {
 	Title = "Time Elapsed",
-	Text = tostring(os.clock() - getgenv().TE)) .. " Seconds;"),
+	Text = tostring((os.clock() - getgenv().TE) .. " Seconds;"),
 	Duration = 10,
 })
 
